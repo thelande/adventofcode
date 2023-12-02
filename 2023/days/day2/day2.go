76fe6e99b9c239695cc8e8acae6c17b4d5bec1db
Tom Helander <thomas.helander@gmail.com>
2023-12-02 11:20:24 -0800

    Extract line parsing to separate function

--- 2023/days/day2/day2.go
+++ 2023/days/day2/day2.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 
 	"github.com/go-kit/log"
-	"github.com/go-kit/log/level"
 	util "github.com/thelande/adventofcode/common"
 )
 
@@ -15,49 +14,58 @@ type Reveal struct {
 }
 
 type Day2 struct {
-	Blue, Red, Green, Possible int64
+	Blue, Red, Green, Possible, TotalPower int64
+}
+
+func parseLine(line string) (int64, []Reveal) {
+	var reveals []Reveal
+
+	re := regexp.MustCompile(`^Game ([0-9]+): (.+)$`)
+	matches := re.FindStringSubmatch(line)
+	gameId, err := strconv.ParseInt(matches[1], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	revealsStr := strings.Split(matches[2], ";")
+	for _, reveal := range revealsStr {
+		r := Reveal{}
+
+		cubes := strings.Split(reveal, ",")
+		for _, cube := range cubes {
+			parts := strings.Split(strings.Trim(cube, " "), " ") // count and color
+			count, err := strconv.ParseInt(parts[0], 10, 64)
+			if err != nil {
+				panic(err)
+			}
+
+			switch parts[1] {
+			case "blue":
+				r.Blue = count
+			case "red":
+				r.Red = count
+			case "green":
+				r.Green = count
+			}
+		}
+
+		reveals = append(reveals, r)
+	}
+
+	return gameId, reveals
 }
 
 func (d Day2) Part1(filename string, logger log.Logger) int64 {
-	d.Blue = 14
 	d.Red = 12
 	d.Green = 13
+	d.Blue = 14
 
-	re := regexp.MustCompile(`^Game ([0-9]+): (.+)$`)
 	util.ReadPuzzleInput(filename, logger, func(line string) error {
 		possible := true
-		matches := re.FindStringSubmatch(line)
-		gameId, err := strconv.ParseInt(matches[1], 10, 64)
-		if err != nil {
-			panic(err)
-		}
-
-		reveals := strings.Split(matches[2], ";")
+		gameId, reveals := parseLine(line)
 		for _, reveal := range reveals {
-			cubes := strings.Split(reveal, ",")
-			for _, cube := range cubes {
-				parts := strings.Split(strings.Trim(cube, " "), " ") // count and color
-				count, err := strconv.ParseInt(parts[0], 10, 64)
-				if err != nil {
-					panic(err)
-				}
-
-				level.Debug(logger).Log("color", parts[1], "count", count)
-
-				switch parts[1] {
-				case "blue":
-					if count > d.Blue {
-						possible = false
-					}
-				case "red":
-					if count > d.Red {
-						possible = false
-					}
-				case "green":
-					if count > d.Green {
-						possible = false
-					}
-				}
+			if reveal.Blue > d.Blue || reveal.Green > d.Green || reveal.Red > d.Red {
+				possible = false
 			}
 		}
 
@@ -72,5 +80,5 @@ func (d Day2) Part1(filename string, logger log.Logger) int64 {
 }
 
 func (d Day2) Part2(filename string, logger log.Logger) int64 {
-	return 0
+	return d.TotalPower
 }
