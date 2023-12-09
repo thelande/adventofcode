9babb56409f46778a1145f28ea75d1e6de0a83bc
Tom Helander <thomas.helander@gmail.com>
2023-12-09 10:06:11 -0800

    Extract path finding function

--- 2023/days/day8/day8.go
+++ 2023/days/day8/day8.go
@@ -1,6 +1,7 @@
 package day8
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/go-kit/log"
@@ -26,8 +27,40 @@ func NewNodeFromLine(line string) *Node {
 	return node
 }
 
-func (d Day8) Part1(filename string, logger log.Logger) int64 {
+// Returns the number of steps for the given starting location to reach
+// the given ending location.
+func stepsToEnd(start string, end *string, endRune *rune, directions string, nodes map[string]*Node) int64 {
+	if end == nil && endRune == nil {
+		panic("Neither end nor endRune provided.")
+	}
+
 	var value int64
+	if end != nil {
+		for curr := start; curr != *end; value++ {
+			idx := int(value) % len(directions)
+			dir := directions[idx]
+			if dir == 'L' {
+				curr = nodes[curr].LeftLabel
+			} else {
+				curr = nodes[curr].RightLabel
+			}
+		}
+	} else {
+		for curr := start; rune(curr[2]) != *endRune; value++ {
+			idx := int(value) % len(directions)
+			dir := directions[idx]
+			if dir == 'L' {
+				curr = nodes[curr].LeftLabel
+			} else {
+				curr = nodes[curr].RightLabel
+			}
+		}
+	}
+
+	return value
+}
+
+func (d Day8) Part1(filename string, logger log.Logger) int64 {
 	var directions string
 	nodes := make(map[string]*Node)
 
@@ -44,41 +77,37 @@ func (d Day8) Part1(filename string, logger log.Logger) int64 {
 	})
 
 	level.Debug(logger).Log("directions", directions)
-	for curr := "AAA"; curr != "ZZZ"; value++ {
-		idx := int(value) % len(directions)
-		dir := directions[idx]
-		if dir == 'L' {
-			curr = nodes[curr].LeftLabel
-		} else {
-			curr = nodes[curr].RightLabel
-		}
-	}
-
-	return value
+	end := "ZZZ"
+	return stepsToEnd("AAA", &end, nil, directions, nodes)
 }
 
 func (d Day8) Part2(filename string, logger log.Logger) int64 {
 	var value int64
-	// var directions string
-	// var complete bool = false
-	// nodes := make(map[string]*Node)
+	var directions string
+	nodes := make(map[string]*Node)
 
 	util.ReadPuzzleInput(filename, logger, func(line string, lineno int) error {
-		// if lineno == 0 {
-		// 	directions = line
-		// } else if lineno == 1 {
-		// 	// skip
-		// } else {
-		// 	node := NewNodeFromLine(line)
-		// 	nodes[node.Label] = node
-		// }
+		if lineno == 0 {
+			directions = line
+		} else if lineno == 1 {
+			// skip
+		} else {
+			node := NewNodeFromLine(line)
+			nodes[node.Label] = node
+		}
 		return nil
 	})
 
-	// Loop until we get all paths to finish at the same time.
-	// for value = 0; !complete; value++ {
-	// 	idx := int(value) % len(directions)
-	// }
+	// Find and track the paths
+	var paths []string
+	for k := range nodes {
+		if k[2] == 'A' {
+			paths = append(paths, k)
+		}
+	}
+
+	level.Debug(logger).Log("directions", directions)
+	level.Debug(logger).Log("len(paths)", len(paths), "paths", fmt.Sprintf("%s", paths))
 
 	return value
 }
